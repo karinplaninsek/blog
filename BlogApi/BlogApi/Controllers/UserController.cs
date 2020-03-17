8d6f391863401121d242e1d7d99016507ad6fa75
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2020-03-17 16:54:38 +0100

    Pushing BlogApi to git

---
+++ BlogApi/BlogApi/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlogApi.Models;
+
+namespace BlogApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Usernames = new[]
+        {
+            "jananovak", "marko.polo", "zupanjan", "gregorcmeta", "katarinah"
+        };
+
+        private static readonly string[] Emails = new[]
+        {
+            "jana.novak@gmail.com", "marko.polo@gmail.com", "jan.zupan@gmail.com", "katarina.hrast@gmail.com"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            var random = new Random();
+
+            List<User> user = new List<User>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var newModel = new User
+                {
+                    Id = i + 1,
+                    Username = Usernames[random.Next(Usernames.Length)],
+                    Email = Emails[random.Next(Emails.Length)]
+                };
+                user.Add(newModel);
+            }
+
+            return user;
+        }
+
+        [HttpGet("{number}")]
+        public IEnumerable<User> List(int number)
+        {
+            var random = new Random();
+
+            List<User> user = new List<User>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var newModel = new User
+                {
+                    Id = i + 1,
+                    Username = Usernames[random.Next(Usernames.Length)],
+                    Email = Emails[random.Next(Emails.Length)]
+                };
+                user.Add(newModel);
+            }
+
+            return user;
+        }
+    }
+}
---
+++ BlogApi/BlogApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+
+            List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var newModel = new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(i),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                };
+                weatherForecasts.Add(newModel);
+            }
+
+
+
+            return weatherForecasts;
+        }
+
+        [HttpGet("list/{number}")]
+        public IEnumerable<WeatherForecast> List(int number)
+        {
+            var rng = new Random();
+
+            List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var newModel = new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(i),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                };
+                weatherForecasts.Add(newModel);
+            }
+
+            return weatherForecasts;
+        }
+    }
+}
